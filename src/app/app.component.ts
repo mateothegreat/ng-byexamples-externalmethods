5bc31ba24f55b55922b250f8ca625b2940b10ddd
Matthew Davis <matthew@matthewdavis.io>
2019-06-08 13:32:35 -0500

    added method

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: [ './app.component.css' ]
 })
-export class AppComponent {
-  title = 'ng-byexamples-externalmethods';
+export class AppComponent implements OnInit, OnDestroy {
+
+    public ngOnInit() {
+
+        window[ 'app' ] = window[ 'app' ] || {};
+        window[ 'app' ].namespace = window[ 'app' ].namespace || {};
+
+        window[ 'app' ].namespace.myAngularMethod = this.myAngularMethod.bind(this);
+
+    }
+
+    public ngOnDestroy() {
+
+        window[ 'app' ].namespace.messageSend = null;
+        window[ 'app' ].namespace.gameStart = null;
+
+    }
+
+    public myAngularMethod(args: any): void {
+
+        console.log('myAngularMethod', args);
+
+    }
+
 }
